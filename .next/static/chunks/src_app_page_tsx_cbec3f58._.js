684d5be4401617bb3fb57f968ee45c8ce4455718
Hidde van Brussel <hidde.vanbrussel@xential.nl>
2026-02-11 08:48:38 +0100

    Add new pages and update app build manifests

--- .next/static/chunks/src_app_page_tsx_cbec3f58._.js
+++ .next/static/chunks/src_app_page_tsx_cbec3f58._.js
@@ -1,8 +1,8 @@
 (globalThis.TURBOPACK_CHUNK_LISTS || (globalThis.TURBOPACK_CHUNK_LISTS = [])).push({
     script: typeof document === "object" ? document.currentScript : undefined,
     chunks: [
-  "static/chunks/node_modules_2440778a._.js",
-  "static/chunks/src_ae9f0286._.js"
+  "static/chunks/node_modules_723af8ca._.js",
+  "static/chunks/src_b56d2bb4._.js"
 ],
     source: "dynamic"
 });
