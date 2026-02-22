897a1b0e3eec6e326356165f70517c929b3127f6
Hidde van Brussel <hidde.vanbrussel@xential.nl>
2026-02-22 20:22:59 +0100

    disable update from app

--- src/app/rooms/page.tsx
+++ src/app/rooms/page.tsx
@@ -352,7 +352,7 @@ export default function RoomsPage() {
                     {getFloorLabel(floor || "")}
                   </h3>
                 )}
-                <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 xl:grid-cols-6 gap-6 sm:gap-8">
+                <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 xl:grid-cols-6 gap-8 sm:gap-10">
                   {floorRooms.map((r) => (
                     <RoomPreviewCard
                       key={r.areaId}
